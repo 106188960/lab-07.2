cd9be331f5c257b7e36ce18f638f8c330a7fb2c2
106188960 <106188960@student.swin.edu.au>
2025-10-02 16:02:43 +1000

    added processbooking.php

---
+++ processbooking.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Booking Confirmation</title>
+	<meta charset="utf-8">
+	<meta name="description" content="Rohirrim Booking Form" >
+	<meta name="keywords"    content="MiddleEarth, Tours, Rohan" >
+	<meta name="author"      content="Grima Wormtongue" />
+</head>
+<body>
+    <header>
+        <h1>Rohirrim Tour Booking Confirmation</h1>
+
+<?php
+        function sanitise_input($data){
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $firstname = sanitise_input($_POST["firstname"]);
+            $lastname = sanitise_input($_POST["lastname"]);
+            $age = sanitise_input($_POST["age"]);
+            $bookday = sanitise_input($_POST["bookday"]);
+            $partysize = sanitise_input($_POST["partysize"]);
+
+        }
+        $species = isset($_POST["species"]);
+        $accom = isset($_POST["accom"]) ? $_POST["accom"] : [];
+        $food = isset($_POST["food"]);
+
+        echo "$firstname\n";
+        echo "$lastname\n";
+        echo "$age\n";
+        echo "$bookday\n";
+        echo "$partysize\n";
+        echo "$food\n"
+
+?>
+
+
+</body>
